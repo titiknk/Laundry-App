e6cee5f9421b5897789af598dcb66c0c1b10085d
titiknk <titikhofifa@gmail.com>
2022-12-22 14:31:30 +0700

    Initial commit

---
+++ app/src/main/java/com/titikhofifa/laundryapp/ActivitySplash.java
@@ -0,0 +1,34 @@
+package com.titikhofifa.laundryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.window.SplashScreen;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(decorView.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        if (getSupportActionBar() != null){
+            getActionBar().hide();
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ActivitySplash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+
+}
