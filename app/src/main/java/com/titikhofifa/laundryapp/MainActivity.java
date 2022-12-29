91a8b1c96c8837824a9d73ef5e3e5ecb241b44fa
AlfanMuhaimin <alfanmuhaimin10@gmail.com>
2022-12-29 12:07:00 +0700

    Menambahkan Array List

--- app/src/main/java/com/titikhofifa/laundryapp/MainActivity.java
+++ app/src/main/java/com/titikhofifa/laundryapp/MainActivity.java
@@ -31,17 +31,27 @@ public class MainActivity extends AppCompatActivity implements LaundryRecycleVie
 
     private void getData() {
         laundryModel = new ArrayList<>();
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Laundry 2",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
-        laundryModel.add(new LaundryModel("Washy washy",R.drawable.pyro));
+        laundryModel.add(new LaundryModel("Washy Washy One Stop Professional Laundry Pandaan",R.drawable.washy_laundry));
+        laundryModel.add(new LaundryModel("Cuci Express",R.drawable.bg_cuci));
+        laundryModel.add(new LaundryModel("Lana Laundry Pandaan",R.drawable.lana_laundry));
+        laundryModel.add(new LaundryModel("Daffa Laundry Pandaan",R.drawable.bg_daffa));
+        laundryModel.add(new LaundryModel("Ardefa Laundry",R.drawable.bg_ardefa));
+        laundryModel.add(new LaundryModel("Eka Laundry",R.drawable.bg_eka));
+        laundryModel.add(new LaundryModel("Wati Laundry 1",R.drawable.bg_wati));
+        laundryModel.add(new LaundryModel("Halo Laundry",R.drawable.bg_halo));
+        laundryModel.add(new LaundryModel("Pinguin Laundry",R.drawable.bg_pinguin));
+        laundryModel.add(new LaundryModel("Di@ Laundry",R.drawable.bg_dia));
+        laundryModel.add(new LaundryModel("Cuci Karpet Mega Laundry 2",R.drawable.bg_mega));
+        laundryModel.add(new LaundryModel("Barokah Laundry Bu Iklima",R.drawable.bg_barokah));
+        laundryModel.add(new LaundryModel("Bintang Laundry",R.drawable.bg_bintang));
+        laundryModel.add(new LaundryModel("AA Laundry & Cuci Karpet",R.drawable.bg_aa));
+        laundryModel.add(new LaundryModel("Puji Laundry",R.drawable.bg_puji));
+        laundryModel.add(new LaundryModel("Kirei Express Laundry",R.drawable.bg_kirei));
+        laundryModel.add(new LaundryModel("Sari Laundry",R.drawable.bg_sari));
+        laundryModel.add(new LaundryModel("Panji Laundry",R.drawable.bg_panji));
+        laundryModel.add(new LaundryModel("Lingga Laundry",R.drawable.bg_lingga));
+
+
     }
 
     @Override
