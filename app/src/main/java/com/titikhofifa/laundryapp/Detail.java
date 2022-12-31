9856d54f5fd5561da5b2c7ee1a97935cdd02c791
Dicky Prasetyo <dickpra15@gmail.com>
2022-12-31 22:53:45 +0700

    Membuat button whatsapp dan maps di detail aplikasi agar bisa di tekan menuju aplikasi tsb, seperti mengirim pesan ke toko laundry melalui whastapp dan membuka lokasi toko laundry di maps.

--- app/src/main/java/com/titikhofifa/laundryapp/Detail.java
+++ app/src/main/java/com/titikhofifa/laundryapp/Detail.java
@@ -1,7 +1,11 @@
 package com.titikhofifa.laundryapp;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,9 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Detail extends AppCompatActivity {
 
-
+    ImageView whatsappBTN, mapsBTN;
     ImageView img;
     TextView judulText, detailText;
+    TextView waText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,10 @@ public class Detail extends AppCompatActivity {
         img = findViewById(R.id.img);
         judulText = findViewById(R.id.judulText);
         detailText = findViewById(R.id.detailText);
+        whatsappBTN = findViewById(R.id.wabtn);
+        mapsBTN = findViewById(R.id.mapsbtn);
+        waText = findViewById(R.id.watext);
+
 
         Intent intent = getIntent();
         int image = intent.getExtras().getInt("Logo");
@@ -33,6 +42,37 @@ public class Detail extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+
+        whatsappBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String url = "https://api.whatsapp.com/send?phone=" + waText.getText().toString();
+                try {
+                    PackageManager pm = getApplicationContext().getPackageManager();
+                    pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                } catch (PackageManager.NameNotFoundException e){
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                }
+            }
+        });
+
+        mapsBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String loc = judulText.getText().toString();
+                Uri locationUri = Uri.parse("geo:0,0?q=" + loc);
+                Intent intent = new Intent(Intent.ACTION_VIEW, locationUri);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 
     @Override
@@ -40,4 +80,8 @@ public class Detail extends AppCompatActivity {
         finish();
         return true;
     }
+
+
+
+
 }
